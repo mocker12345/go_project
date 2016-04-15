9a49f5aa71d1b58d00f3c880ec94abb28fa5f8ad
rancongjie <95633676@qq.com>
2016-04-15 17:04:13 +0800

    mygolangbolg first commit

---
+++ src/myweb/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	"myweb/models"
+	_ "myweb/routers"
+)
+
+func init() {
+	models.RegisterDB()
+}
+func main() {
+	orm.Debug = true
+	orm.RunSyncdb("default", false, true)
+	beego.Run()
+}
